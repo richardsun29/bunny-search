ac83a4085851b6aef01f161afec209f1b77633a1
richardsun29 <richardsun29@ucla.edu>
2015-06-14 03:42:04 -0700

    make progress bar

--- geturls.sh
+++ geturls.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Array of queries to search
-declare -a queries=('bunny' 'rabbit' 'cute bunny')
+declare -a queries=('bunny' 'rabbit' 'cute bunny' 'bunny gif')
+#declare -a queries=('bunny')
 
-# Number of items to search for
+# Number of items to search for (rounds now to a multiple of 8)
 nitems=100
 
 # Replace spaces with URL escape characters
@@ -22,13 +23,29 @@ done
 
 # Get image URLs
 for query in ${queries[@]}; do
+	printf "Retrieving search results for %s..." $query
 	urls=""
 	for ((i = 0; i < nitems; i+=8)); do
+		# Loading percent
+		percent=$(bc <<< "scale = 3; $i/$nitems*100" | awk -F. '{print $1}')
+		printf "%.0f%%" $percent
+
 		urls+=$(curl -s "http://ajax.googleapis.com/ajax/services/search/images?q=$query&v=1.0&start=$i&rsz=8" | 
 				sed 's/,/\n/g' | grep unescapedUrl | awk -F'"' '{print $4}')
 		urls+=" "
+
+#		[ $percent -ne 0 ] && printf "\b\b"
+#		[ $percent -ge 10 ] && printf "\b"
+		if [ $percent -lt 10 ]; then
+			printf "\b\b"
+		else
+			printf "\b\b\b"
+		fi
 	done
 	echo "$urls" | sed 's/ /\n/g' > queries/query-"$query".txt
+
+	# Clean up loading bar
+	printf "100%%\n"
 done
 
 
